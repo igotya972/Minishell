589c66643d5730fa32436f01a84cb2b710b50dc4
afont1 <afont@student.42nice.fr>
2024-02-19 13:14:25 +0100

    plus aucun leaks sur les builtins et le lexer

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: afont <afont@student.42nice.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 19:38:04 by dferjul           #+#    #+#             */
-/*   Updated: 2024/02/19 11:42:50 by afont            ###   ########.fr       */
+/*   Updated: 2024/02/19 13:13:37 by afont            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,5 @@ int	main(int argc, char **argv, char **envp)
 	return (0);
 }
 
-// to do : proteger les mallocs
-// to do : leaks quand je fais export salut=salut
 // to do : prise en compte des pipes dans le lexer et ailleurs 
-// to do : prise en compte des simple quotes dans le lexer
 // to do : affiche double prompt quand tu fait wc -l
